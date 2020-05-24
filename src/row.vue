4ffeea3d3582b285ce48cd9df6402cfc0d14357a
fieedw <frankding8843@gmail.com>
2020-05-25 02:56:34 +0800

    模仿淘宝网布局代码备注

--- src/row.vue
+++ src/row.vue
@@ -1,32 +1,42 @@
 <template>
-    <div :style="rowStyle" class="row">
+    <div :class="rowClass" :style="rowStyle" class="row">
         <slot></slot>
     </div>
 </template>
 <script>
     export default {
-        name:'GuLu-row',
-        props:{
-            gutter:{
-                type:[Number,String]
+        name: 'GuLu-row',
+        props: {
+            gutter: {
+                type: [Number, String]
+            },
+            align:{
+              type:[Number,String]  ,
+                validator(value){
+                  return ['right','left','content'].includes(value)
+                }
             }
         },
         //页面打印空对象，创建于内存
         created() {
-  console.log('row created')
+            console.log('row created')
         },
         //页面有对象，创建并显示于body
         mounted() {
             console.log('row mounted ')
-            this.$children.forEach((vm)=>{
-                vm.gutter =this.gutter
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
             })
         },
-        computed:{
-            rowStyle(){
-                let {gutter} =this
-                return {marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}
-             // return   {marginLeft:-(this.gutter/2+'px'),marginRight:-(this.gutter/2+'px')}
+        computed: {
+            rowStyle() {
+                let {gutter} = this
+                return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+                // return   {marginLeft:-(this.gutter/2+'px'),marginRight:-(this.gutter/2+'px')}
+            },
+            rowClass() {
+                let {align} = this
+                return [align && `align-${align}`]
             }
         }
     }
@@ -36,7 +46,16 @@
     // document.body.appendChild(div)
 </script>
 <style lang="scss" scoped>
-    .row{
+    .row {
         display: flex;
+        &.align-left{
+            justify-content: flex-start;
+        }
+        &.align-right{
+            justify-content: flex-end;
+        }
+        &.align-content{
+            justify-content: center;
+        }
     }
 </style>
