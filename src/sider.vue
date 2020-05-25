8caecb9f1a74029e9cab9e7ec8e93f92b52cf260
fieedw <frankding8843@gmail.com>
2020-05-26 00:20:27 +0800

    添加侧边栏关闭

--- src/sider.vue
+++ src/sider.vue
@@ -1,13 +1,31 @@
 <template>
-    <div class="sider">
+    <transition name="fade">
+    <div class="sider" v-if="visible">
         <slot></slot>
+        <button @click="visible=false">close</button>
     </div>
+    </transition>
 </template>
 <script>
     export  default {
-        name:'GuLuSider'
+        name:'GuLuSider',
+        data(){
+          return{
+              visible:true
+          }
+        },
+        methods:{}
     }
 </script>
 <style lang="scss" scoped>
-
+.sider{
+    position:relative;
+    > button{
+        position: absolute;
+        top:10px;
+        right:10px;
+    }
+}
+.fade-enter-active, .fade-leave-active{transition: all .5s;}
+    .fade-enter , .fade-leave-to{margin-left: -200px;}
 </style>
