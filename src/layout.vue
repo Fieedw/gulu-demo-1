aee82bea633b567c37c5ba1407341edceeea26ab
fieedw <frankding8843@gmail.com>
2020-05-25 22:37:28 +0800

    上中下基础布局

---
+++ src/layout.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="layout">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GuLu-layout',
+        data() {
+            return {
+                layoutClass: {
+                    hasSider: false
+                }
+            }
+        },
+        mounted() {
+            this.$children.forEach(() => {
+                if (vm.$options.name === 'GuLuSider') {
+                    this.layoutClass.hasSider = true
+                }
+            })
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .layout {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        /*border: 1px solid red;*/
+        &.hasSider {
+            flex-direction: row;
+        }
+    }
+</style>
