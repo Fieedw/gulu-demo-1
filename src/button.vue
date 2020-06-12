0084e900ca5fdaa62ad555ab6a51b5b33d697016
fieedw <frankding8843@gmail.com>
2020-06-12 14:00:32 +0800

    完成collapse组件及测试用例

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,4 @@
 <template>
-    <!--    {[iconPosition]:true}-->
-</template>
-<template>
     <button :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')" class="g-button">
         <g-icon :name="icon" class="icon" v-if="icon && !loading"></g-icon>
         <g-icon class="loading icon" name="loading" v-if="loading"></g-icon>
@@ -30,34 +27,38 @@
                 type: String,
                 default: 'left',
                 validator: function (value) {
-                    if (value !== 'left' && value !== 'right') {
-                        return false
-                    } else {
-                        return true
-                    }
+                    return !(value !== 'left' && value !== 'right');
                 }
             }
         }
     }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+    $button-height: 32px;
+    $font-size: 14px;
+    $button-background: white;
+    $button-active-background: #eee;
+    $border-radius: 4px;
+    $color: #333;
+    $border-color: #999;
+    $border-color-hover: #666;
     .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
+        font-size: $font-size;
+        height: $button-height;
         padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-background);
+        border-radius: $border-radius;
+        border: 1px solid $border-color;
+        background: $button-background;
         display: inline-flex;
         justify-content: center;
         align-items: center;
         vertical-align: middle; //内联元素对齐
         &:hover {
-            border-color: var(--border-color-hover)
+            border-color: $border-color-hover
         }
 
         &:active {
-            background-color: var(--button-active-background);
+            background-color:$button-active-background;
         }
 
         &:focus {
